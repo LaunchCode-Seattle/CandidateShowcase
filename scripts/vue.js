b37a1855dd868913d5f79b81a95e479cac012766
Benjamin Shutt <ben@launchcode.org>
2018-02-07 12:00:42 -0800

    Tweaked the repo information fields, added default data, tweaked the page organization and styling

--- scripts/vue.js
+++ scripts/vue.js
@@ -1,13 +1,29 @@
+function defaultData() {
+	var json = {};
+	json.groups = [];
+	json.groups[0] = {}
+	json.groups[0].location = "City";
+	json.groups[0].number = "5";
+	json.groups[0].repo = "repoLink";
+	json.groups[0].members = [];
+	json.groups[0].members[0] = {};
+	json.groups[0].members[0].firstName = "";
+	json.groups[0].members[0].lastName = "";
+	json.groups[0].members[0].userName = "";
+	return JSON.stringify(json);
+}
 function loadData(callback) {
 	var request = new XMLHttpRequest();
 	request.overrideMimeType("application/json");
 	request.open("GET", "data.json", true);
 	request.onreadystatechange = function() {
-		if (request.readyState == 4 && request.status == 200) {
-			callback(request.responseText);
+		if (request.readyState == 4) {
+			if (request.status == 200) {
+				callback(reqvouest.responseText);
+			}
 		}
 	}
-	request.send(null);
+	callback(defaultData());
 }
 
 function vueOnLoad(raw) {
@@ -20,4 +36,6 @@ function vueOnLoad(raw) {
 	});
 }
 
-window.onload = loadData(vueOnLoad);
+window.onload = function() {
+	loadData(vueOnLoad);
+}

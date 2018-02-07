39986dabb40ecce4418bed21aef05c676bec5d32
Benjamin Shutt <ben@launchcode.org>
2018-02-06 19:11:51 -0800

    Added Vue and Bulma

---
+++ scripts/vue.js
@@ -0,0 +1,22 @@
+function loadData(callback) {
+	var request = new XMLHttpRequest();
+	request.overrideMimeType("application/json");
+	request.open("open", "data.json", true);
+	request.onreadystatechange = function() {
+		console.log("readyState == " & request.readyState);
+		if (request.readyState == 4 && request.status == 200) {
+			callback(request.responseText);
+		}
+	}
+	request.send(null);
+}
+
+function vueOnLoad(raw) {
+	console.log(raw)
+	new Vue({
+		el: '#app',
+		data: JSON.parse(raw)
+	});
+}
+
+window.onload = loadData(vueOnLoad);

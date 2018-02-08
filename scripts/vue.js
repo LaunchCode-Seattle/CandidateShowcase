379368352b022ace081aaf5c70eac42ef98c3863
Benjamin Shutt <ben@launchcode.org>
2018-02-07 17:06:55 -0800

    Removed extra styling for now. Fixed the data parsing bug.

--- scripts/vue.js
+++ scripts/vue.js
@@ -4,7 +4,8 @@ function defaultData() {
 	json.groups[0] = {}
 	json.groups[0].location = "City";
 	json.groups[0].number = "5";
-	json.groups[0].repo = "repoLink";
+	json.groups[0].repoLocation = "username";
+	json.groups[0].repoName = "Repo Name";
 	json.groups[0].members = [];
 	json.groups[0].members[0] = {};
 	json.groups[0].members[0].firstName = "";
@@ -13,18 +14,23 @@ function defaultData() {
 	return JSON.stringify(json);
 }
 function loadData(callback) {
-	var request = new XMLHttpRequest();
-	request.overrideMimeType("application/json");
-	request.open("GET", "data.json", true);
-	request.onreadystatechange = function() {
-		if (request.readyState == 4) {
-			if (request.status == 200) {
-				callback(request.responseText);
+	if (location.hostname === "")
+	{
+		callback(defaultData());
+	}
+	else {
+		var request = new XMLHttpRequest();
+		request.overrideMimeType("application/json");
+		request.open("GET", "data.json", true);
+		request.onreadystatechange = function() {
+			if (request.readyState == 4) {
+				if (request.status == 200) {
+					callback(request.responseText);
+				}
 			}
 		}
+		request.send(null);
 	}
-	console.error("Could not load data. Falling back to default data.");
-	callback(defaultData());
 }
 
 function vueOnLoad(raw) {
@@ -33,7 +39,12 @@ function vueOnLoad(raw) {
 	}
 	new Vue({
 		el: '#app',
-		data: data
+		data: data,
+		methods: {
+			repoUrl: function() {
+				return "http://github.com/" + this.repoLocation + "/" + this.repoName;
+			}
+		}
 	});
 }
 
